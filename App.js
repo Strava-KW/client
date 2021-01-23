c10febb162c77d185f97d2db7ec5a08a85da51a6
robbykansas <lomiver@gmail.com>
2021-01-23 09:46:51 +0700

    maps

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { StyleSheet } from 'react-native';
+// import RunTracker from './src/components/RunTracker'
+import EventLocation from './src/components/EventLocation'
 
+const Stack = createStackNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={EventLocation}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
