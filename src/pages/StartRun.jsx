21b60315a165e3a635ae2ffa4ab4427d8f3ad748
muchsin28 <hab@muchsin.id>
2021-01-24 09:07:30 +0700

    OAuth Google

--- src/pages/StartRun.jsx
+++ src/pages/StartRun.jsx
@@ -1,47 +1,48 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
-import { RunTracker } from '../components'
-import { Button } from 'react-native-paper'
-import { MaterialIcons } from '@expo/vector-icons'; 
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { RunTracker } from "../components";
+import { Button } from "react-native-paper";
+import { MaterialIcons } from "@expo/vector-icons";
 
-function StartRun () {
+function StartRun({ route, Navigation }) {
   return (
     <>
       <View>
-        <RunTracker/>
+        <RunTracker />
         <View
           style={{
-              position: 'absolute',//use absolute position to show button on top of the map
-              top: '60%', //for center align
-              alignSelf: 'center' //for align to right
+            position: "absolute", //use absolute position to show button on top of the map
+            top: "60%", //for center align
+            alignSelf: "center", //for align to right
           }}
         >
           <Button
             mode="contained"
             color="#FA8135"
-            compact={true} 
+            compact={true}
             style={styles.playButton}
             contentStyle={{
               height: 70,
-              width: 70
+              width: 70,
             }}
             labelStyle={{
-              fontFamily: 'Jost',
-              fontSize: 16
+              fontFamily: "Jost",
+              fontSize: 16,
             }}
+            onPress={() => console.log(route)}
           >
             START
           </Button>
         </View>
       </View>
     </>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   playButton: {
-    borderRadius: 100
-  }
-})
+    borderRadius: 100,
+  },
+});
 
-export default StartRun
+export default StartRun;
