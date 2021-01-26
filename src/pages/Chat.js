1f5277c61ff9da1446be0f5ff68eaa1bd217a432
agnespravida <agnespravida27@gmail.com>
2021-01-26 22:40:28 +0700

    setup firebase

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,66 +1,73 @@
-import React, {useEffect, useState} from 'react'
+// @refresh reset
+import React, {useEffect, useState, useCallback} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { GiftedChat } from 'react-native-gifted-chat'
 import AsyncStorage from '@react-native-community/async-storage'
-import firebase from 'firebase'
+import * as firebase from 'firebase'
 import 'firebase/firestore'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchCommunity } from '../store/actions'
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAq3OSehcmiJOgw6VC4IkolVrP4MR6L7fM",
-//   authDomain: "runator-h8.firebaseapp.com",
-//   projectId: "runator-h8",
-//   storageBucket: "runator-h8.appspot.com",
-//   messagingSenderId: "716222498981",
-//   appId: "1:716222498981:web:faf4a24bc7087a1ba3e54c"
-// };
+var firebaseConfig = {
+  apiKey: "AIzaSyBQkRqgO6jXK6aEGk0KWFJm5nb0fffv9Xc",
+  authDomain: "runator-chat.firebaseapp.com",
+  databaseURL: "https://runator-chat-default-rtdb.firebaseio.com",
+  projectId: "runator-chat",
+  storageBucket: "runator-chat.appspot.com",
+  messagingSenderId: "783820234678",
+  appId: "1:783820234678:web:03bb37b05762277b0ceb17",
+  databaseURL: "https://runator-chat-default-rtdb.firebaseio.com/"
+};
 
-// if (firebase.apps.length === 0 ){
-//   firebase.initializeApp(firebaseConfig);
-// } 
-
-// const db = firebase.firestore()
+if (firebase.apps.length === 0 ){
+  firebase.initializeApp(firebaseConfig);
+} 
 
 export default function Chat() {
+  
+  const db = firebase.firestore()
   const dispatch = useDispatch()
   const communities = useSelector(state => state.communities)
   const access_token = useSelector(state => state.access_token)
-  // const [user, setUser] = useState(null)
-
+  const profile = useSelector(state => state.profile)
+  const [user, setUser] = useState({id: null, name: null})
+  const [messages, setMessages] = useState([])
+  let chatsRef;
+  if (communities) {
+    chatsRef = db.collection(communities._id.toString())
+  }
   useEffect(() => {
     if (access_token) {
       dispatch(fetchCommunity(access_token))
     }
-    // axios users
-    // Name and setName with initial useSelector from axios 
-    // if (communities && users) {
-    //   const chatsRef = db.collection('chats')
-    // }
-    if (chatsRef) {
+    if (profile) {
+      setUser({id: profile._id.toString(), name: profile.fullname})
+    }
+  }, [access_token, profile])
+
+  useEffect(() => {
+    if(chatsRef) {
       const unsubscribe = chatsRef.onSnapshot((querySnapshot) => {
         const messagesFirestore = querySnapshot
-            .docChanges()
-            .filter(({ type }) => type === 'added')
-            .map(({ doc }) => {
-                const message = doc.data()
-                return { ...message, createdAt: message.createdAt.toDate() }
-            })
-            .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+        .docChanges()
+        .filter(({ type }) => type === 'added')
+        .map(({ doc }) => {
+          const message = doc.data()
+          return { ...message, createdAt: message.createdAt.toDate() }
+        })
+        .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
         appendMessages(messagesFirestore)
-    })
-    return () => unsubscribe()
+      })
+      return () => unsubscribe()
     }
-  }, [access_token])
+  }, [chatsRef])
 
-  const appendMessages = useCallback(
-    (messages) => {
+  const appendMessages = useCallback((messages) => {
         setMessages((previousMessages) => GiftedChat.append(previousMessages, messages))
     },
     [messages]
   )
-
-  function handleSend(messages) {
+  async function handleSend(messages) {
     const writes = messages.map((m) => chatsRef.add(m))
     await Promise.all(writes)
   }
